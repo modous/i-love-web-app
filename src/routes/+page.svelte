b2b174f3aece0a640d035d57444380b9e012df7c
modous <mohamed1010@live.nl>
2023-12-17 17:10:24 +0100

    kleine beschrijving + verandering kleur + styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,53 +10,71 @@
 			<h1>Scrum crash course</h1>
 			<time datetime="29/05/2023">29/05/2023</time>
 			<img src="/assets/scrum.jpg" alt="scrum" />
-			<h2>Notes</h2>
-			<p>Important fundamentals:</p>
+			<h2>- Notes -</h2>
+			<p>
+				Lars gaf vandaag een presentatie in het eerstejaarscollege over hoe ze bij Funda de sprint
+				review aanpakken en welke scrum fundamentals ze daarvoor gebruiken.
+			</p>
+			<p class="headline">Important fundamentals:</p>
 			<ul>
-				<li>Sprint reviews</li>
-				<li>Sprint retrospective</li>
-				<li>Daily Scrum</li>
+				<li>- Sprint reviews</li>
+				<li>- Sprint retrospective</li>
+				<li>- Daily Scrum</li>
+				<li>- Backlog refilement</li>
+				<li>- Sprint planning</li>
+				<li>- Backlog refinement</li>
 			</ul>
-			<p>Scrum team composition:</p>
+			<p class="headline">Scrum team composition:</p>
 			<ul>
-				<li>Developers</li>
-				<li>Scrum master</li>
-				<li>Product owner</li>
+				<li>- Developers</li>
+				<li>- Scrum master</li>
+				<li>- Product owner</li>
 			</ul>
+			<textarea name="" id="" cols="30" rows="10">typ hier je aantekeningen!</textarea>
 		</section>
 
 		<section>
 			<h1>Code Meetup</h1>
 			<time datetime="06/03/2023">06/03/2023</time>
 			<img src="/assets/code.jpg" alt="code" />
-			<h2>Notes</h2>
-			<p>Speakers:</p>
+
+			<h2>- Notes -</h2>
+			<p>
+				Ik was vandaag bij een Code meetup bij Digital IO waar mijn broer werkt. Hier kwamen 2
+				sprekers uit America(Kent C. Dodds) en Belgie(Antionne Poiret).
+			</p>
+			<p class="headline">Speakers:</p>
 			<ul>
-				<li>Kent C. Dodds</li>
-				<li>Antoinne Poiret</li>
+				<li>- Kent C. Dodds</li>
+				<li>- Antoinne Poiret</li>
 			</ul>
-			<p>Subject:</p>
+			<p class="headline">Subject:</p>
 			<ul>
-				<li>Remix</li>
-				<li>Qwik</li>
-				<li>Server-side rendering</li>
+				<li>- Remix</li>
+				<li>- Qwik</li>
+				<li>- Server-side rendering</li>
 			</ul>
+			<textarea name="" id="" cols="30" rows="10">Typ hier je aantekeningen!</textarea>
 		</section>
 
 		<section>
 			<h1>Event</h1>
 			<time datetime="06/03/2023">Date</time>
 
-			<img src="/assets/code.jpg" alt="code" />
-			<h2>Notes</h2>
+			<img src="" alt="img" />
+			<h2>- Notes -</h2>
+
+			<textarea name="" id="" cols="30" rows="10">Typ hier je aantekeningen!</textarea>
 		</section>
 
 		<section>
 			<h1>Event</h1>
 			<time datetime="06/03/2023">Date</time>
 
-			<img src="/assets/code.jpg" alt="code" />
-			<h2>Notes</h2>
+			<img src="" alt="img" />
+			<h2>- Notes -</h2>
+
+			<textarea name="" id="" cols="30" rows="10">Typ hier je aantekeningen!</textarea>
 		</section>
 	</main>
 </body>
@@ -70,9 +88,13 @@
 		color: black;
 	}
 
-    .We-Love-Web:hover {
+	.We-Love-Web:hover {
 		transform: scale(1.2);
-        transition-duration: 2s;
+		transition-duration: 2s;
+	}
+
+	.headline {
+		font-weight: bold;
 	}
 
 	main {
@@ -88,6 +110,7 @@
 		margin: 0;
 		background-color: #a2dd2c;
 		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+		border-radius: 1rem;
 	}
 
 	h1 {
@@ -96,7 +119,8 @@
 	}
 
 	h2 {
-		color: #4a90e2; /* Blue text color */
+		/* color: #4a90e2;  */
+		color: rgb(89, 44, 214);
 	}
 
 	img {
