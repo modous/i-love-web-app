11a1f613634e1488abcd6d4fa2cb7a90cab8b15b
modous <mohamed1010@live.nl>
2024-01-19 12:29:44 +0100

    added keyframes and connected the hygraph api :fire:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,19 @@
 </script>
 
 <body>
-	<h1 class="We-Love-Web">We Love Web</h1>
+	<h1 class="We-Love-Web">
+		<span>W</span>
+		<span>e</span>
+		<span> </span>
+		<span>L</span>
+		<span>o</span>
+		<span>v</span>
+		<span>e</span>
+		<span> </span>
+		<span>W</span>
+		<span>e</span>
+		<span>b</span>
+	</h1>
 
 	<main>
 		<section>
@@ -88,6 +100,51 @@
 		color: black;
 	}
 
+	.We-Love-Web span {
+		display: inline-block;
+		animation: spin 1s;
+	}
+
+	.We-Love-Web span:nth-child(1) {
+		animation-delay: 0.1s;
+	}
+	.We-Love-Web span:nth-child(2) {
+		animation-delay: 0.2s;
+	}
+	.We-Love-Web span:nth-child(4) {
+		animation-delay: 0.3s;
+	}
+	.We-Love-Web span:nth-child(5) {
+		animation-delay: 0.4s;
+	}
+	.We-Love-Web span:nth-child(5) {
+		animation-delay: 0.5s;
+	}
+	.We-Love-Web span:nth-child(6) {
+		animation-delay: 0.6s;
+	}
+	.We-Love-Web span:nth-child(7) {
+		animation-delay: 0.7s;
+	}
+	.We-Love-Web span:nth-child(9) {
+		animation-delay: 0.8s;
+	}
+	.We-Love-Web span:nth-child(10) {
+		animation-delay: 0.9s;
+	}
+	.We-Love-Web span:nth-child(11) {
+		animation-delay: 1s;
+	}
+
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+
 	.We-Love-Web:hover {
 		transform: scale(1.2);
 		transition-duration: 2s;
@@ -116,6 +173,18 @@
 		background-color: #a2dd2c;
 		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 		border-radius: 1rem;
+		animation: grow 2s;
+	}
+
+	@keyframes grow {
+		0% {
+			opacity: 0;
+			transform: scale(0);
+		}
+		100% {
+			opacity: 100%;
+			transform: scale(1);
+		}
 	}
 
 	h1 {
